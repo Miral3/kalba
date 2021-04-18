641cacb9728366c296af58830f7e5db9b824aaa0
YongSangYou <dydtkd113@gmail.com>
2021-04-18 16:39:00 +0900

    react-router 활용하여 구성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React from 'react';
-import Header from './components/layouts/Header/Header';
+import { BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+
+import './app.css'
+
+import RouteWithLayout from './layouts/RouteWithLayout'
+import MainLayout from './layouts/Main';
+
+import Home from './routes/Home';
+import LeaderBoards from './routes/LeaderBoards';
+import StandardTable from './routes/StandardTable';
+import Profile from './routes/Profile';
+import NotFoundPage from './routes/NotFoundPage';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <Router>
+      <Switch>
+        <Redirect
+          exact
+          from="/"
+          to="/home"
+        />
+        <RouteWithLayout
+          path="/home"
+          layout={MainLayout}
+          component={Home}
+        />
+        <RouteWithLayout
+          path="/leaderBoards/:category?"
+          layout={MainLayout}
+          component={LeaderBoards}
+        />
+        <RouteWithLayout
+          path="/standardTable/:category?"
+          layout={MainLayout}
+          component={StandardTable}
+        />
+        <RouteWithLayout
+          path="/profile/:category?"
+          layout={MainLayout}
+          component={Profile}
+        />
+        <RouteWithLayout
+          path="/not-found"
+          layout={MainLayout}
+          component={NotFoundPage}
+        />
+        <Redirect to="/not-found" />
+      </Switch>
+    </Router>
   );
 };
 
