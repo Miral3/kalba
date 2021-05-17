864ac5c2e51c9755a7e623fa596ef028a0e7f341
potionk <potionkr@gmail.com>
2021-05-17 15:12:08 +0900

    Add db setting info in config

--- kalba-rest-api/src/main/java/kalba/config/ReadConfig.java
+++ kalba-rest-api/src/main/java/kalba/config/ReadConfig.java
@@ -10,6 +10,14 @@ public class ReadConfig {
     public Object isLocal;
     public Object localToken;
     public Object serverToken;
+    public Object dbHost;
+    public Object dbPort;
+    public Object dbUser;
+    public Object dbPassword;
+    public Object dbSchema;
+    public Object mysqlMinConnection;
+    public Object mysqlMaxConnection;
+
 
     private static ReadConfig fromFile() {
         try {
@@ -18,6 +26,13 @@ public class ReadConfig {
             config.isLocal= Boolean.parseBoolean(config.isLocal.toString());
             config.localToken=config.localToken.toString();
             config.serverToken=config.serverToken.toString();
+            config.dbHost=config.dbHost.toString();
+            config.dbPort= Integer.parseInt(config.dbPort.toString());
+            config.dbUser=config.dbUser.toString();
+            config.dbPassword=config.dbPassword==null?"":config.dbPassword.toString();
+            config.dbSchema=config.dbSchema.toString();
+            config.mysqlMaxConnection=Integer.parseInt(config.mysqlMaxConnection.toString());
+            config.mysqlMinConnection=Integer.parseInt(config.mysqlMinConnection.toString());
             reader.close();
             return config;
         } catch (FileNotFoundException e) {
