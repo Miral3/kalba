546af8f3e401186db1a667f2d5e1182d571b5182
potionk <potionkr@gmail.com>
2021-12-15 15:21:01 +0900

    Add getLoginUserTag function in tools

--- src/tools/tools.js
+++ src/tools/tools.js
@@ -112,6 +112,10 @@ export function getLoginUserNickname() {
   return window.localStorage.getItem('nickname');
 }
 
+export function getLoginUserTag() {
+  return window.localStorage.getItem('tag');
+}
+
 export function getLoginToken() {
   return isEmpty(window.localStorage.getItem('token')) ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwb3Rpb25kZXYiLCJleHAiOjE2Mjk4ODk2NDksImlhdCI6MTYyOTg3MTY0OX0.1G0XJBpMy2QWiGiI9mIhfagKpIOi8uFM2k0hxZFKTyMo6vN3OE0B17Xa-u9k6u1aDpqWkTTLkaIFQAgJhkHd-g" : window.localStorage.getItem('token');
 }
