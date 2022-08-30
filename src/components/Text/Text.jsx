87634dd0cf07641e9252839faac2fc90358fd04e
YongSangYou <dydtkd113@gmail.com>
2022-08-30 21:04:18 +0900

    feat: Text 컴포넌트 구현

---
+++ src/components/Text/Text.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { PropTypes } from "prop-types";
+import * as S from "./Text.style";
+import Common from "../../styles/common";
+
+const propTypes = {
+  children: PropTypes.node.isRequired,
+  size: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  color: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  weight: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+};
+
+const defaultProps = {
+  size: Common.fontSize.h[2],
+  color: Common.colors.black[0],
+  weight: "normal",
+};
+
+const Text = ({ children, size, color, weight }) => {
+  return (
+    <S.Text size={size} color={color} weight={weight}>
+      {children}
+    </S.Text>
+  );
+};
+
+Text.propTypes = propTypes;
+Text.defaultProps = defaultProps;
+
+export default Text;
