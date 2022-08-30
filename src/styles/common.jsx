9a1fb9e17034362a3da9ee70768dca3473822929
YongSangYou <dydtkd113@gmail.com>
2022-08-30 20:14:04 +0900

    design: 전역 스타일

---
+++ src/styles/common.jsx
@@ -0,0 +1,35 @@
+const Common = {
+  colors: {
+    black: ["#000000", "#1C1C20", "#171715"],
+    white: ["#FFFFFF"],
+    gary: [
+      "#F5F5F5",
+      "#F0F0F0",
+      "#848484",
+      "#E6E6E6",
+      "#D6D6D6",
+      "#616b8a",
+      "#9496c8",
+    ],
+    indigo: ["#383F4C", "#272B35", "#2D2D38", "#252530", "#28282F", "#31313D"],
+    brown: ["#5E4A3E"],
+    basic: ["#E5E2D6"],
+    red: ["#DA2A29", "#E93636"],
+    purple: ["#6a7497", "#42376c"],
+  },
+  gradient: {
+    gradient: [
+      "linear-gradient(0deg,#6d779a,#8c96ae)",
+      "linear-gradient(0deg,#49417a,#635c91)",
+    ],
+  },
+  fontSize: {
+    t: ["36px"],
+    h: ["24px", "20px", "15px"],
+    b: ["19px", "16px", "13px"],
+    c: ["11px", "12px"],
+    l: ["45px", "35px", "32px"],
+  },
+};
+
+export default Common;
