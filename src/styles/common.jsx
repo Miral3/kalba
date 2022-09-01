4f183a306178df21d632d5cc70414c99c0835555
YongSangYou <dydtkd113@gmail.com>
2022-09-01 16:24:16 +0900

    design: common 변수 추가

--- src/styles/common.jsx
+++ src/styles/common.jsx
@@ -14,7 +14,7 @@ const Common = {
     indigo: ["#383F4C", "#272B35", "#2D2D38", "#252530", "#28282F", "#31313D"],
     brown: ["#5E4A3E"],
     basic: ["#E5E2D6"],
-    red: ["#DA2A29", "#E93636"],
+    red: ["#DA2A29", "#E93636", "#FF4500"],
     purple: ["#6a7497", "#42376c"],
   },
   gradient: {
@@ -26,11 +26,12 @@ const Common = {
   fontSize: {
     t: ["36px"],
     h: ["24px", "20px", "15px"],
-    b: ["19px", "16px", "13px"],
+    b: ["18px", "16px", "13px"],
     c: ["11px", "12px"],
     l: ["45px", "35px", "32px"],
   },
   display: {
+    mobile: "425px",
     tablet: "768px",
     laptop: "1020px",
     desktop: "1400px",
