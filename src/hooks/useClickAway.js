d4063e37b0943eea5d98ad64d5e7ecd4d2cc5d0e
YongSangYou <dydtkd113@gmail.com>
2022-09-22 20:53:12 +0900

    feat: useClickAway 커스텀 훅 구현

---
+++ src/hooks/useClickAway.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-restricted-syntax */
+import { useEffect, useRef } from "react";
+
+const events = ["mousedown", "touchstart"];
+
+const useClickAway = (handler) => {
+  const ref = useRef(null);
+  const savedHandler = useRef(handler);
+
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+
+  useEffect(() => {
+    const element = ref.current;
+    if (!element) return;
+
+    const handleEvent = (e) => {
+      if (!element.contains(e.target)) {
+        savedHandler.current(e);
+      }
+    };
+
+    for (const eventName of events) {
+      document.addEventListener(eventName, handleEvent);
+    }
+
+    return () => {
+      for (const eventName of events) {
+        document.removeEventListener(eventName, handleEvent);
+      }
+    };
+  }, [ref]);
+
+  return ref;
+};
+
+export default useClickAway;
