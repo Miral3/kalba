f6dd2bd3ea73a1646509f16e3fcf2c52dc02edb3
YongSangYou <dydtkd113@gmail.com>
2022-09-22 20:34:56 +0900

    refactor: NavLink대신 "onClick "prop을 받음

--- src/components/AutoComplete/AutoComplete.jsx
+++ src/components/AutoComplete/AutoComplete.jsx
@@ -6,18 +6,21 @@ const propTypes = {
   data: PropTypes.instanceOf(Array).isRequired,
   active: PropTypes.number.isRequired,
   visible: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
-const AutoComplete = forwardRef(({ data, active, visible }, ref) => {
+const AutoComplete = forwardRef(({ data, active, visible, onClick }, ref) => {
   return (
     <S.Container visible={visible}>
       <S.List ref={ref}>
         {data.map((item, idx) => (
-          <S.Item key={item.tag} active={active === idx}>
-            <S.Link to={`/profile/${item.tag}`}>
-              <S.Name>{item.name}</S.Name>
-              <S.Tag> - {item.tag}</S.Tag>
-            </S.Link>
+          <S.Item
+            key={item.tag}
+            active={active === idx}
+            onClick={() => onClick(item)}
+          >
+            <S.Name>{item.name}</S.Name>
+            <S.Tag> - {item.tag}</S.Tag>
           </S.Item>
         ))}
       </S.List>
