eb16e2ce349e3b75edd014b1dc5030697f0ed43d
YongSangYou <dydtkd113@gmail.com>
2022-09-28 19:19:00 +0900

    docs: 프로필 imgPath

---
+++ src/pages/Profile/data/imgPath.js
@@ -0,0 +1,5 @@
+export const heroesSrcPath = "/img/coc/coc_Heroes";
+export const petsSrcPath = "/img/coc/coc_Pets";
+export const troopsSrcPath = "/img/coc/coc_Troops";
+export const spellsSrcPath = "/img/coc/coc_Spells";
+export const siegeMachinesSrcPath = "/img/coc/coc_SiegeMachines";
