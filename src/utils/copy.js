3d2916d2adcb191d108971fae71160403d63b3be
YongSangYou <dydtkd113@gmail.com>
2022-10-03 18:23:39 +0900

    refactor: 복사 함수 예외처리

--- src/utils/copy.js
+++ src/utils/copy.js
@@ -3,10 +3,19 @@ export const copyText = (text) => {
     navigator.clipboard
       .writeText(text)
       .then(() => {
-        alert("클립보드에 복사되었습니다.");
+        alert(`${text}가 클립보드에 복사되었습니다.`);
       })
       .catch(() => {
         alert("다시 시도해주세요.");
       });
+  } else {
+    const textarea = document.createElement("textarea");
+    document.body.appendChild(textarea);
+    textarea.value = text;
+    textarea.focus();
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+    alert(`${text}가 클립보드에 복사되었습니다.`);
   }
 };
