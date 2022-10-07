ff5852a33eb56cbed12ba2e21b679ddbcb9423f0
YongSangYou <dydtkd113@gmail.com>
2022-10-07 21:14:21 +0900

    feat: Management 컴포넌트 구현

---
+++ src/pages/Admin/components/Management/Management.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import { managementTableColumns } from "../../../../assets/data";
+import { Table } from "../../../../components";
+import { accountInfo } from "../../../../assets/dummyData";
+import * as S from "./Management.style";
+
+const Management = () => {
+  const [loading, setLoading] = useState(true);
+  const [tableData, setTableData] = useState([]);
+  const [isInitialized, setIsInitialized] = useState(false);
+
+  const handleChangeState = (tag, type, e) => {
+    const { checked } = e.target;
+    setTableData(
+      tableData.map((row) =>
+        tag === row.tag
+          ? {
+              ...row,
+              [type]: checked,
+            }
+          : row
+      )
+    );
+    setIsInitialized(true);
+  };
+
+  useEffect(() => {
+    const res = [...accountInfo];
+    setTableData(res);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (!isInitialized) return;
+    const fetchData = async () => {
+      /**
+       * @Todo tableData가 변할때 마다 account info 업데이트
+       */
+    };
+    fetchData();
+  }, [tableData]);
+
+  if (loading) {
+    return;
+  }
+
+  return (
+    <S.Main>
+      <S.Section>
+        <S.Container>
+          <Table
+            version="management"
+            columns={managementTableColumns}
+            data={tableData}
+            handleChangeState={handleChangeState}
+          />
+        </S.Container>
+      </S.Section>
+    </S.Main>
+  );
+};
+
+export default Management;
---
+++ src/pages/Admin/components/Management/Management.style.js
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+import Common from "../../../../styles/common";
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  justify-content: center;
+  padding-top: 24px;
+  padding-bottom: 80px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+
+  ${Common.mediaQuery.tabletS} {
+    width: 540px;
+  }
+
+  ${Common.mediaQuery.tablet} {
+    width: 720px;
+  }
+`;
