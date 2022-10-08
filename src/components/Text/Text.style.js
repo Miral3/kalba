ca492211ededd0c7c8253824bf8a4d40c6e2651a
YongSangYou <dydtkd113@gmail.com>
2022-10-08 21:01:48 +0900

    design: color 변경

--- src/components/Text/Text.style.js
+++ src/components/Text/Text.style.js
@@ -3,5 +3,5 @@ import styled from "@emotion/styled";
 export const Text = styled.span`
   font-size: ${({ size }) => size};
   font-weight: ${({ weight }) => weight};
-  color: ${({ color }) => color};
+  color: inherit;
 `;
