fc6d6df1405d2f90033c2046db10f500267a2f32
YongSangYou <dydtkd113@gmail.com>
2022-10-15 17:18:13 +0900

    chore: recoil 추가

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
