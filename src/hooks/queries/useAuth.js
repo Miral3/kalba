e681cfec6081a0e944f3040ac2475b0f3eb537ac
YongSangYou <dydtkd113@gmail.com>
2022-11-02 21:07:40 +0900

    feat: 로컬스토리징에 token 저장

--- src/hooks/queries/useAuth.js
+++ src/hooks/queries/useAuth.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useMutation } from "react-query";
+import useLocalStorage from "../useLocalstorage";
 import * as url from "../../constants/apiUrl";
 
 export const useLogin = ({ options }) => {
   const navigate = useNavigate();
+  const [, setToken] = useLocalStorage("token", null);
   return useMutation((info) => axios.post(`${url.LOGIN}`, info), {
     onSuccess({ data }) {
       navigate(`/`);
-      console.log(data.token);
+      setToken(data.token);
     },
     onError(err) {
       alert("계정 정보가 올바르지 않습니다. 다시 한번 확인해주세요.");
