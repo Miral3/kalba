fc8a29a4c26791a573952e583dac02172e447d7a
YongSangYou <dydtkd113@gmail.com>
2022-11-05 19:08:25 +0900

    feat: isLoading prop 추가

--- src/components/AutoComplete/AutoComplete.jsx
+++ src/components/AutoComplete/AutoComplete.jsx
@@ -3,30 +3,36 @@ import PropTypes from "prop-types";
 import * as S from "./AutoComplete.style";
 
 const propTypes = {
+  isLoading: PropTypes.bool.isRequired,
   data: PropTypes.instanceOf(Array).isRequired,
   active: PropTypes.number.isRequired,
   visible: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
 };
 
-const AutoComplete = forwardRef(({ data, active, visible, onClick }, ref) => {
-  return (
-    <S.Container visible={visible}>
-      <S.List ref={ref}>
-        {data.map((item, idx) => (
-          <S.Item
-            key={item.tag}
-            active={active === idx}
-            onClick={() => onClick(item)}
-          >
-            <S.Name>{item.name}</S.Name>
-            <S.Tag> - {item.tag}</S.Tag>
-          </S.Item>
-        ))}
-      </S.List>
-    </S.Container>
-  );
-});
+const AutoComplete = forwardRef(
+  ({ isLoading, data, active, visible, onClick }, ref) => {
+    if (isLoading || !data) {
+      return;
+    }
+    return (
+      <S.Container visible={visible}>
+        <S.List ref={ref}>
+          {data.map((item, idx) => (
+            <S.Item
+              key={item.tag}
+              active={active === idx}
+              onClick={() => onClick(item)}
+            >
+              <S.Name>{item.name}</S.Name>
+              <S.Tag> - {item.tag}</S.Tag>
+            </S.Item>
+          ))}
+        </S.List>
+      </S.Container>
+    );
+  }
+);
 
 AutoComplete.propTypes = propTypes;
 
