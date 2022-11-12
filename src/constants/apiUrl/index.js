ab33d9a0bb1c377cc1333e2de29fd6e49238d394
YongSangYou <dydtkd113@gmail.com>
2022-11-12 17:17:21 +0900

    feat: CLAN_MEMBER api url 추가

--- src/constants/apiUrl/index.js
+++ src/constants/apiUrl/index.js
@@ -13,3 +13,4 @@ export const RANK = "/coc/clan/rank";
 export const RANK_UPDATE = "/coc/clan/update";
 export const FORMULA = "/coc/clan/formula";
 export const PROFILE = "/coc/clan/member/statistic/tag";
+export const CLAN_MEMBER = "/coc/clan/member/statistic/all";
