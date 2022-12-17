1190a55f2a657024b06a52da34cbb937580fdb77
YongSangYou <dydtkd113@gmail.com>
2022-12-17 17:11:09 +0900

    design: 버튼 디자인

--- src/pages/Admin/components/Management/Management.style.js
+++ src/pages/Admin/components/Management/Management.style.js
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import Common from "../../../../styles/common";
+import { Button } from "../../../../components";
 
 export const Container = styled.div`
   display: flex;
@@ -13,5 +14,25 @@ export const Container = styled.div`
 
   ${Common.mediaQuery.tablet} {
     width: 720px;
+  }
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 16px;
+`;
+
+export const StyledButton = styled(Button)`
+  border: ${({ theme }) => theme.border.tr};
+  padding: 5px 10px;
+  ${({ dir }) => dir === "left" && "margin-right: auto"};
+  background-color: ${({ theme }) => theme.bg.tbody};
+  color: ${({ theme }) => theme.text.tbody};
+  font-weight: 600;
+  font-size: ${Common.fontSize.b[2]};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.hover.tbody};
   }
 `;
