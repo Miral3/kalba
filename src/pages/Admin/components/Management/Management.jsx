7ac2bcf9c1c650fea1453a9928533a4205d681ac
YongSangYou <dydtkd113@gmail.com>
2022-12-18 14:10:02 +0900

    feat: api 적용

--- src/pages/Admin/components/Management/Management.jsx
+++ src/pages/Admin/components/Management/Management.jsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { useClanMember } from "../../../../hooks/queries/useClanMember";
+import {
+  useClanMember,
+  useClanMemberUpdate,
+} from "../../../../hooks/queries/useClanMember";
 import { managementTableColumns } from "../../../../assets/data";
 import { Spinner, Table } from "../../../../components";
 import * as S from "./Management.style";
 
 const Management = () => {
   const { isLoading, data } = useClanMember({});
+  const { mutate } = useClanMemberUpdate({});
   const [tableData, setTableData] = useState([]);
+  const [updatedData, setUpdatedData] = useState(new Map());
   const openChatState = ["NOT_MEMBER", "MEMBER", "SUB_LEADER"];
-  // const openChatState = ["X", "O", "부방장"];
 
   const handleChangeOpenChatState = (state, tag) => {
     if (state === "LEADER") {
@@ -25,9 +29,29 @@ const Management = () => {
         : row
     );
     setTableData(nextTableData);
-    /**
-     * @Todo nextTableData 서버에 저장
-     */
+
+    const updatedTagToSelected = new Map(updatedData);
+    updatedTagToSelected.set(tag, nextState);
+    setUpdatedData(updatedTagToSelected);
+  };
+
+  const handleSaveTableData = () => {
+    if (!updatedData.size) {
+      return;
+    }
+    const openChatStates = Array.from(updatedData, ([tag, openChatState]) => ({
+      tag,
+      openChatState,
+    }));
+    mutate(openChatStates);
+  };
+
+  const handleInitTable = () => {
+    if (JSON.stringify(data) === JSON.stringify(tableData)) {
+      return;
+    }
+    setTableData(data);
+    alert("초기화 되었습니다.");
   };
 
   useEffect(() => {
@@ -48,6 +72,14 @@ const Management = () => {
           handleChangeOpenChatState={handleChangeOpenChatState}
         />
       )}
+      <S.Actions>
+        <S.StyledButton onClick={() => handleSaveTableData()}>
+          저장
+        </S.StyledButton>
+        <S.StyledButton onClick={() => handleInitTable()}>
+          초기화
+        </S.StyledButton>
+      </S.Actions>
     </S.Container>
   );
 };
